b36f31a12c6cc67cf1150b94564a4b50cb3fc015
Christopher Johnstone <meson800@gmail.com>
2024-04-05 13:40:12 -0400

    Rename miRNA deg coefficent instead of "unbind"

--- modeling/src/miR_iFFL.jl
+++ modeling/src/miR_iFFL.jl
@@ -15,7 +15,7 @@ dicer_risc_rn = @reaction_network dicer_RISC_complex begin
         r_dicer=1e-3            # pre-miRNA processing rate constant [1/s]
         δ_mi=2.88e-4            # miRNA degradation rate constant [1/s]
         k_miRNA_bind=1e-5       # miRNA-RISC binding rate constant [1/mol*s]
-        k_miRNA_unbind=2.16e-5  # miRNA-RISC unbinding rate constant [1/s]
+        k_miRNA_deg=2.16e-5  # miRNA-RISC unbinding rate constant [1/s]
         k_mRNA_bind=1.84e-6     # RISC complex formation rate constant[1/mol*s]
         k_mRNA_unbind=0.303     # RISC complex deformation rate constant [1/s]
         k_deg=7e-3              # RISC-mediated mRNA degradation rate constant [1/s]
@@ -26,7 +26,7 @@ dicer_risc_rn = @reaction_network dicer_RISC_complex begin
     δ_mi, miRNA --> ∅
     # Knockdown
     k_miRNA_bind, risc + miRNA --> risc_miRNA
-    k_miRNA_unbind, risc_miRNA --> risc
+    k_miRNA_deg, risc_miRNA --> risc
     (k_mRNA_bind, k_mRNA_unbind), risc_miRNA + regulated_mRNA <--> risc_miRNA_mRNA
     k_deg, risc_miRNA_mRNA --> risc_miRNA
 end
@@ -137,7 +137,7 @@ single_iFFL_rn = @reaction_network single_transcript_iFFL begin
         r_drosha=1e-2 #pri-miRNA processing rate constant [1/s] 
         r_dicer=1e-3 #pre-miRNA processing rate constant [1/s]
         k_miRNA_bind=1e-5 #miRNA-RISC binding rate constant [1/mol*s]
-        k_miRNA_unbind=2.16e-5 #miRNA-RISC unbinding rate constant [1/s]
+        k_miRNA_deg=2.16e-5 #miRNA-RISC unbinding rate constant [1/s]
         k_mRNA_bind=1.84e-6 #RISC complex formation rate constant[1/mol*s]
         k_mRNA_unbind=0.303 #RISC complex deformation rate constant [1/s]
         k_deg=7e-3 #RISC-mediated mRNA degradation rate constant [1/s]
@@ -159,7 +159,7 @@ single_iFFL_rn = @reaction_network single_transcript_iFFL begin
     r_dicer, pre_miRNA --> miRNA # dicer
     # Knockdown
     k_miRNA_bind, risc + miRNA --> risc_miRNA
-    k_miRNA_unbind, risc_miRNA --> risc
+    k_miRNA_deg, risc_miRNA --> risc
     (k_mRNA_bind, k_mRNA_unbind), risc_miRNA + mRNA <--> risc_miRNA_mRNA
     k_deg, risc_miRNA_mRNA --> risc_miRNA
 end
@@ -183,7 +183,7 @@ double_iFFL_rn = @reaction_network double_transcript_iFFL begin
         r_drosha=1e-2 #pri-miRNA processing rate constant [1/s] 
         r_dicer=1e-3 #pre-miRNA processing rate constant [1/s]
         k_miRNA_bind=1e-5 #miRNA-RISC binding rate constant [1/mol*s]
-        k_miRNA_unbind=2.16e-5 #miRNA-RISC unbinding rate constant [1/s]
+        k_miRNA_deg=2.16e-5 #miRNA-RISC unbinding rate constant [1/s]
         k_mRNA_bind=1.84e-6 #RISC complex formation rate constant[1/mol*s]
         k_mRNA_unbind=0.303 #RISC complex deformation rate constant [1/s]
         k_deg=7e-3 #RISC-mediated mRNA degradation rate constant [1/s]
@@ -209,7 +209,7 @@ double_iFFL_rn = @reaction_network double_transcript_iFFL begin
     r_dicer, pre_miRNA --> miRNA # dicer
     # Knockdown
     k_miRNA_bind, risc + miRNA --> risc_miRNA
-    k_miRNA_unbind, risc_miRNA --> risc
+    k_miRNA_deg, risc_miRNA --> risc
     (k_mRNA_bind, k_mRNA_unbind), risc_miRNA + mRNA1 <--> risc_miRNA_mRNA
     k_deg, risc_miRNA_mRNA --> risc_miRNA
 end
@@ -227,7 +227,7 @@ U6_iFFL_rn = @reaction_network double_transcript_U6_iFFL begin
         ζ=0.0 #Translation efficiency for RISC complex [unitless], Range: 0 to 1
         r_dicer=1e-3 #pre-miRNA processing rate constant [1/s]
         k_miRNA_bind=1e-5 #miRNA-RISC binding rate constant [1/mol*s]
-        k_miRNA_unbind=2.16e-5 #miRNA-RISC unbinding rate constant [1/s]
+        k_miRNA_deg=2.16e-5 #miRNA-RISC unbinding rate constant [1/s]
         k_mRNA_bind=1.84e-6 #RISC complex formation rate constant[1/mol*s]
         k_mRNA_unbind=0.303 #RISC complex deformation rate constant [1/s]
         k_deg=7e-3 #RISC-mediated mRNA degradation rate constant [1/s]
@@ -246,7 +246,7 @@ U6_iFFL_rn = @reaction_network double_transcript_U6_iFFL begin
     r_dicer, pre_miRNA --> miRNA # dicer
     # Knockdown
     k_miRNA_bind, risc + miRNA --> risc_miRNA
-    k_miRNA_unbind, risc_miRNA --> risc
+    k_miRNA_deg, risc_miRNA --> risc
     (k_mRNA_bind, k_mRNA_unbind), risc_miRNA + mRNA1 <--> risc_miRNA_mRNA
     k_deg, risc_miRNA_mRNA --> risc_miRNA
 end
