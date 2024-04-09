336a615e7bd55f19d910dbb897f57d443bd055be
Christopher Johnstone <meson800@gmail.com>
2024-04-09 17:57:18 -0400

    Updated with analysis of changing params

--- modeling/src/steady_state_analysis.jl
+++ modeling/src/steady_state_analysis.jl
@@ -6,13 +6,8 @@ using Latexify
 outdir = "$(@__DIR__)/../../output/modeling/latex"
 mkpath(outdir)
 
-variable_replacements
-
-single_transcript_iFFL
-open("$(outdir)/single_transcript_odes.tex", "w") do file
-    ode_system_latex = latexify(convert(ODESystem, single_transcript_iFFL))
-    mapped = replace(ode_system_latex,
-       raw"\mathrm{pre}_{miRNA}\left( t \right)" => raw"\text{pre}(t)",
+variable_replacements = (
+    raw"\mathrm{pre}_{miRNA}\left( t \right)" => raw"\text{pre}(t)",
        raw"\mathrm{d} \mathrm{miRNA}\left( t \right)" => raw"\text{miR}(t)",
        raw"\mathrm{risc}\left( t \right)" => raw"\text{R}(t)",
        raw"\mathrm{risc}_{miRNA}\left( t \right)" => raw"\text{RI}(t)",
@@ -28,7 +23,18 @@ open("$(outdir)/single_transcript_odes.tex", "w") do file
        "miRNA" => "miR",
        "unbind" => "ub",
        "bind" => "b",
-    )
+)
+
+open("$(outdir)/single_transcript_odes.tex", "w") do file
+    ode_system_latex = latexify(convert(ODESystem, single_transcript_iFFL))
+    mapped = replace(ode_system_latex, variable_replacements...)
     reordered = getindex(split(mapped, "\n"), [1, 9, 10, 2, 3, 4, 5, 7, 6, 8, 11])
+    write(file, join(reordered, "\n"))
+end
+
+open("$(outdir)/shared_odes.tex", "w") do file
+    ode_system_latex = latexify(convert(ODESystem, single_transcript_iFFL))
+    mapped = replace(ode_system_latex, variable_replacements...)
+    reordered = getindex(split(mapped, "\n"), [1, 2, 3, 4, 5, 7, 6, 8, 11])
     write(file, join(reordered, "\n"))
 end
