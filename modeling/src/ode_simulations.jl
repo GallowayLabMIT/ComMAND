51139143a8499259fdd7a77fab01d9972507b075
Christopher Johnstone <meson800@gmail.com>
2024-04-13 17:09:30 -0400

    Update ode simulation endpoint

--- modeling/src/ode_simulations.jl
+++ modeling/src/ode_simulations.jl
@@ -6,8 +6,8 @@ using CairoMakie
 system = convert(ODESystem, single_transcript_iFFL)
 
 problem = ODEProblem(single_transcript_iFFL, 
-    [:pre_miRNA => 0, :miRNA => 0, :risc => 100, :risc_miRNA => 0, :regulated_mRNA => 0, :risc_miRNA_mRNA => 0, :protein => 0, :immature_mRNA => 0, :pri_miRNA => 0],
-    (0., 100000.)
+    [:pre_miRNA => 0, :miRNA => 0, :risc => 170000, :risc_miRNA => 0, :regulated_mRNA => 0, :risc_miRNA_mRNA => 0, :protein => 0, :immature_mRNA => 0, :pri_miRNA => 0],
+    (0., 300000.)
 )
 
 soln = solve(problem, Tsit5())
