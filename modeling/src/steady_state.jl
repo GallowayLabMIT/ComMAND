95c0912710b345aff244c81344dd0a1672131fac
Christopher Johnstone <meson800@gmail.com>
2024-04-13 17:07:01 -0400

    Added steady state computation of mRNA from other parameters

--- modeling/src/steady_state.jl
+++ modeling/src/steady_state.jl
@@ -1,32 +1,63 @@
+using miR_iFFL
+using Catalyst
 import PolynomialRoots
+using Makie
+using CairoMakie
 
 param_dict = Dict(Symbol(k) => v for (k,v) ∈ ModelingToolkit.get_defaults(single_transcript_iFFL))
 
 # RI = R / (κ1 + κ2 R)
 # RIM = R / (κ3 + κ4 R)
 pre(p) = p[:r_splicing] / p[:r_dicer] * (p[:regulated_copy] * p[:α_im]) / (p[:δ_im] + p[:r_splicing])
+r_regulated(p) = p[:regulated_copy] * p[:α_im]
 κ1(p) = (p[:k_miRNA_deg] * p[:δ_mi]) / (p[:k_miRNA_bind] * p[:r_dicer] * pre(p))
 κ2(p) = (p[:k_miRNA_deg]) / (p[:r_dicer] * pre(p))
-κ3(p) = (p[:δ_m] * p[:δ_mi] * p[:k_miRNA_deg]) * (p[:k_deg] + p[:k_mRNA_unbind]) / (p[:k_mRNA_bind] * p[:k_miRNA_bind] * p[:r_dicer] * pre(p))
-κ4(p) = p[:δ_m] * p[:k_miRNA_deg] * (p[:k_deg] + p[:k_mRNA_unbind]) / (p[:k_mRNA_bind] * p[:r_dicer] * pre(p)) + p[:k_deg]
+κ3(p) = (p[:δ_m] * p[:δ_mi] * p[:k_miRNA_deg]) * (p[:k_deg] + p[:k_mRNA_unbind]) / (p[:k_mRNA_bind] * p[:k_miRNA_bind] * p[:r_dicer] * pre(p) * r_regulated(p))
+κ4(p) = p[:δ_m] * p[:k_miRNA_deg] * (p[:k_deg] + p[:k_mRNA_unbind]) / (p[:k_mRNA_bind] * p[:r_dicer] * pre(p) * r_regulated(p)) + (p[:k_deg] / r_regulated(p))
+mRNA(R, p) = r_regulated(p) / (
+        p[:δ_m] + 
+            (p[:k_deg] * p[:k_mRNA_bind] * p[:k_miRNA_bind] * p[:r_dicer] * pre(p) * R)
+            / (p[:k_miRNA_deg] * (p[:k_deg] + p[:k_mRNA_unbind]) * (p[:δ_mi] + p[:k_miRNA_bind] * R))
+    )
 # Then
 # So Rtot = R + RI + RIM
 # Rtot (κ1 + κ2 R) (κ3 + κ4 R) = R (κ1 + κ2 R) (κ3 + κ4 R) + (κ3 + κ4 R) R + (κ1 + κ2 R) R
 # 0 = κ2 κ4 R^3 + κ1 κ4 R^2 + κ2 κ3 R^2 + κ1 κ3 R + κ4 R^2 + κ3 R + κ2 R^2 + κ1 R - Rtot κ2 κ4 R^2 - Rtot κ1 κ4 R - Rtot κ2 κ3 R - Rtot κ1 κ3
 # (κ2 κ4) R^3 + (κ1 κ4 + κ2 κ3 + κ4 + κ2 - Rtot κ2 κ4) R^2 + (κ1 κ3 + κ3  + κ1 - Rtot κ1 κ4 - Rtot κ2 κ3) R - Rtot κ1 κ3
-function find_R_val(params)
+function find_R_vals(params)
     κ = [κ1(params), κ2(params), κ3(params), κ4(params)]
     Rtot = 170000
-    roots = PolynomialRoots.roots([
-        κ[2] * κ[4],
-        κ[1] * κ[4] + κ[2] * κ[3] + κ[4] + κ[2] - Rtot * κ[2] * κ[4],
+    # Coeffs ordered from R^0 up to R^3
+    coeffs = [
+        -Rtot * κ[1] * κ[3],
         κ[1] * κ[3] + κ[3] + κ[1] - Rtot * κ[1] * κ[4] - Rtot * κ[2] * κ[3],
-        -Rtot * κ[1] * κ[3]
-    ])
+        κ[1] * κ[4] + κ[2] * κ[3] + κ[4] + κ[2] - Rtot * κ[2] * κ[4],
+        κ[2] * κ[4]
+    ]
+    d, c, b, a = (coeffs[1], coeffs[2], coeffs[3], coeffs[4])
+    discriminant = b * b * c * c - 4 * a * c * c * c - 4 * b * b * b * d - 27 * a * a * b * b + 18 * a * b * c * d
+    println("discriminant: ", discriminant)
+    roots = PolynomialRoots.roots(coeffs)
+    # Return all positive real roots
+    return map(real, filter(x->imag(x)==0.0 && real(x) > 0, roots))
 end
 
 [κ1(param_dict), κ2(param_dict), κ3(param_dict), κ4(param_dict)]
-find_R_val(param_dict)
+R_val = find_R_vals(param_dict)[1]
+mRNA(R_val, param_dict)
+
+function calculate_mRNA(copy_number)
+    params = deepcopy(param_dict)
+    params[:regulated_copy] = copy_number
+    mRNA(find_R_vals(params)[1], params)
+end
+calculate_mRNA(100)
+f = Figure()
+ax = Axis(f[1,1], xlabel="Copy number", ylabel="Steady state mRNA")
+plot!(ax, 1:200, calculate_mRNA.(1:200), xlabel="Copy number", ylabel="Steady state mRNA", title="Copy number sweep")
+hidespines!(ax, :r)
+hidespines!(ax, :t)
+f
 
 """
 \text{RIM} = \frac{k_{mRNA\_b} k_{miR\_b} r_\text{dicer} \cdot \text{pre} \cdot \text{R}}{\delta_m \delta_{mi} k_{miR\_ub} (k_\text{deg} +
